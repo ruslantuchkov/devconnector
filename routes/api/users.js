9f9122e2065914ecc204bf9522e41e2ca23da94c
ruslantuchkov <nidjel87@gmail.com>
2018-05-09 14:40:51 +0500

    login capability and private route with jwt and passport

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+
+const keys = require('../../config/keys');
 const User = require('../../models/User');
 
 router.get('/test', (req, res) => res.json({ msg: 'Users works' }));
@@ -35,5 +39,45 @@ router.post('/register', (req, res) => {
     }
   });
 });
+
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      return res.status(404).json({ email: 'User not found' });
+    } else {
+      bcrypt.compare(password, user.password).then(isMatch => {
+        if (isMatch) {
+          const payload = { id: user.id, name: user.name, avatar: user.avatar };
+
+          jwt.sign(
+            payload,
+            keys.secretOrKey,
+            { expiresIn: 3600 },
+            (err, token) => {
+              res.json({ success: true, token: 'Bearer ' + token });
+            }
+          );
+        } else {
+          return res.status(400).json({ password: 'Password incorrect' });
+        }
+      });
+    }
+  });
+});
+
+router.get(
+  '/current',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user._id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -16,7 +17,8 @@ mongoose
   .then(() => console.log('mongoDB connected'))
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('hello'));
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 app.use('/api/users', users);
 app.use('/api/profile', profile);
