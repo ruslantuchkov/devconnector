15746d988ce0c3edc8fdd61ec4c6c8edbf5622db
ruslantuchkov <nidjel87@gmail.com>
2018-05-11 16:03:05 +0500

    basic client setup with layout

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Navbar from './components/Layout/Navbar';
+import Footer from './components/Layout/Footer';
+import Landing from './components/Layout/Landing';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route path="/" exact component={Landing} />
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
